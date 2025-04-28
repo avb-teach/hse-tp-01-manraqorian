cc1656e04d0a37202f1fb33aff969e0df95b3794
manraqorian <kristinakov2006@yandex.ru>
2025-04-28 12:21:34 +0300

    test

--- collect_files.sh
+++ collect_files.sh
@@ -2,57 +2,36 @@
 
 input_dir="$1"
 output_dir="$2"
-max_depth=""
 
+copy_files() {
+  local source_dir="$1"
 
-if [ "$3" == "--max_depth" ]; then
-    max_depth="$4"
-    shift 2
-fi
+  find "$source_dir" -type f -print0 | while IFS= read -r -d $'\0' file; do
+    filename=$(basename "$file")
 
-collect_files() {
-    local current_dir="$1"
-    local current_depth="$2"
-
-    if [ -n "$max_depth" ] && [ "$current_depth" -gt "$max_depth" ]; then
-        return
-    fi
-
-    find "$current_dir" -maxdepth 1 -type f ! -path "$current_dir" -print0 | while IFS= read -r -d $'\0' file; do
-        filename=$(basename "$file")
-
-        output_file="$output_dir/$filename"
-        if [ -f "$output_file" ]; then
-            i=1
-            name="${filename%.*}"
-            ext="${filename##*.}"
-            while [ -f "$output_dir/${name}_${i}.${ext}" ]; do
-                i=$((i+1))
-            done
-            filename="${name}_${i}.${ext}"
-        fi
-        
-        cp "$file" "$output_dir/$filename"
+    output_file="$output_dir/$filename"
+    i=1
+    while [ -f "$output_file" ]; do
+      name="${filename%.*}"
+      ext="${filename##*.}"
+      output_file="$output_dir/${name}_${i}.${ext}"
+      i=$((i+1))
     done
 
-    find "$current_dir" -maxdepth 1 -type d ! -path "$current_dir" -print0 | while IFS= read -r -d $'\0' dir; do
-        next_depth=$((current_depth + 1))
-        collect_files "$dir" "$next_depth"
-    done
+    cp "$file" "$output_file"
+  done
 }
 
+process_directory() {
+  local dir="$1"
 
-if [ -z "$input_dir" ] || [ -z "$output_dir" ]; then
-    echo "Использование  $0 <input_dir> <output_dir> [--max_depth <depth>]"
-    exit 1
-fi
+  copy_files "$dir"
 
-if [ ! -d "$input_dir" ]; then
-    echo "Ошибочка - данной директории не сущ"
-    exit 1
-fi
-if [ ! -d "$output_dir" ]; then
-    mkdir -p "$output_dir"
-fi
+  find "$dir" -type d -print0 | while IFS= read -r -d $'\0' subdir; do
+    if [ "$subdir" != "$dir" ]; then
+      process_directory "$subdir"
+    fi
+  done
+}
 
-collect_files "$input_dir" 1
+process_directory "$input_dir"
